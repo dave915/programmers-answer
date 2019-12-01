001c48a99ad0354ce8832432eb230179dcc4a025
dave915 <htaehyun@gmail.com>
2019-12-01 23:27:11 +0900

    같은숫자는 싫어 : Stream으로 풀기

--- src/java_answer/Lessons12906.java
+++ src/java_answer/Lessons12906.java
@@ -3,6 +3,7 @@ package java_answer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static java.lang.System.out;
 
@@ -13,28 +14,31 @@ import static java.lang.System.out;
 public class Lessons12906 {
 
     public static void main(String[] args) {
-        int[] arr = {1,1,3,3,0,1,1};
+        int[] arr = {1, 1, 3, 3, 0, 1, 1};
         out.println(Arrays.toString((new Lessons12906()).solution(arr)));
     }
 
-    private int[] solution(int []arr) {
+    private int[] solution(int[] arr) {
         List<Integer> array = new ArrayList<>();
-        for(int i=0; i<arr.length; i++) {
-            if(i == 0) {
-                array.add(arr[i]);
-                continue;
+        int preNum = -1;
+        for (int num : arr) {
+            if (preNum != num) {
+                array.add(num);
             }
-
-            if(arr[i-1] != arr[i])
-                array.add(arr[i]);
-
+            preNum = num;
         }
 
-        int[] answer = new int[array.size()];
-        for(int i=0; i<array.size(); i++){
-            answer[i] = array.get(i);
-        }
+        return array.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
 
-        return answer;
+    private int[] solution2(int[] arr) {
+        return IntStream.range(0, arr.length)
+                .filter(i -> {
+                    if (i == 0) return true;
+                    return arr[i - 1] != arr[i];
+                }).map(i -> arr[i])
+                .toArray();
     }
 }
